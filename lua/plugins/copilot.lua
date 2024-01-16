39a59368e0ec799f29c68cb5ead2d58b7ef5273a
itrepkus <ignas.trepkus@vinted.com>
2024-01-16 19:34:14 +0200

    Add new configs

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,19 @@
 return {
     "zbirenbaum/copilot.lua",
+    dependancies = {
+        "zbirenbaum/copilot-cmp"
+    },
     config = function ()
-        require("copilot").setup({})
+        require("copilot").setup({
+            panel = {
+                enabled = true,
+                auto_refresh = true
+            },
+            filetypes = {
+                ["*"] = true,
+            }
+        })
+
     end,
         cmd = "Copilot",
 }
