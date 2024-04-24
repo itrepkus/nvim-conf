100b206a6e7111f2a9915b6287acfa0189e36530
itrepkus <ignas.trepkus@vinted.com>
2024-04-24 11:29:37 +0300

    add go to definition remap

--- lua/ignas/lsp_remap.lua
+++ lua/ignas/lsp_remap.lua
@@ -3,6 +3,7 @@ local opts = { noremap = true, silent = true }
 
 keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- go to definition
 keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
